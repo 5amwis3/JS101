03d6d28cd13fb85bfd4aab8183199ef7311c79b8
5amwis3 <106936469+5amwis3@users.noreply.github.com>
2022-11-26 14:40:05 -0600

    updated code per suggestions and readability

--- lesson2/rps.js
+++ lesson2/rps.js
@@ -1,9 +1,27 @@
 const readline = require('readline-sync');
 const MESSAGES = require('./rps_messages.json');
-const VALID_CHOICES = ['rock', 'paper','scissors', 'Spock', 'lizard'];
+const VALID_CHOICES = {
+  r: 'rock',
+  p: 'paper',
+  s: 'scissors',
+  S: 'Spock',
+  l: 'lizard'
+};
+const WINNING_MOVES = {
+  rock: ['lizard', 'scissors'],
+  paper: ['rock', 'Spock'],
+  scissors: ['paper', 'lizard'],
+  Spock: ['scissors', 'rock'],
+  lizard: ['Spock', 'paper']
+};
 let winner = '';
-let scoreKeeper = [['User', 0], ['Computer', 0]];
+let scoreKeeper = {
+  user: 0,
+  computer: 0,
+  ties: 0
+};
 let choice = '';
+let computerChoice = '';
 let bestOfFive = 0;
 let answer = '';
 
@@ -16,30 +34,21 @@ function prompt(msg1,msg2) {
 }
 
 function header() {
+  console.clear();
   prompt('header');
-  console.log(`=>        USER: ${scoreKeeper[0][1]} VS COMPUTER: ${scoreKeeper[1][1]}`);
-}
-
-function compareWinner(choice, computerChoice) {
-  return (choice === 'scissors' && computerChoice === 'paper') ||
-         (choice === 'paper' && computerChoice === 'rock') ||
-         (choice === 'rock' && computerChoice === 'lizard') ||
-         (choice === 'lizard' && computerChoice === 'Spock') ||
-         (choice === 'Spock' && computerChoice === 'scissors') ||
-         (choice === 'scissors' && computerChoice === 'lizard') ||
-         (choice === 'lizard' && computerChoice === 'paper') ||
-         (choice === 'paper' && computerChoice === 'Spock') ||
-         (choice === 'Spock' && computerChoice === 'rock') ||
-         (choice === 'rock' && computerChoice === 'scissors');
+  console.log(`=>        USER: ${scoreKeeper.user} VS COMPUTER: ${scoreKeeper.computer}`);
 }
 
 function calculateWinner(choice, computerChoice) {
-  if  (compareWinner(choice, computerChoice)) {
+  if  (WINNING_MOVES[choice].includes(computerChoice)) {
     winner = 'user';
+    scoreKeeper.user += 1;
   } else if (choice === computerChoice) {
     winner = 'tie';
+    scoreKeeper.ties += 1;
   } else {
     winner = 'computer';
+    scoreKeeper.computer += 1;
   }
 }
 
@@ -53,76 +62,69 @@ function displayWinner (whoWon) {
   }
 }
 
-function tally(whoWon) {
-  if (whoWon === 'user') {
-    scoreKeeper[0][1] += 1;
-  } else if (whoWon === 'computer') {
-    scoreKeeper[1][1] += 1;
-  }
+function populateFullChoiceWord(input) {
+  choice = VALID_CHOICES[input] || input;
 }
 
-function populateFullChoiceWord(whatWasChosen) {
-  if (whatWasChosen !== '') {
-    for (let index = 0; index < VALID_CHOICES.length; ++index) {
-      if (VALID_CHOICES[index][0] === whatWasChosen[0].trimStart()) {
-        choice = VALID_CHOICES[index];
-      }
-    }
-  }
-}
-
-console.clear();
-
-//repeats upon user request
-while (bestOfFive < 5) {
-  header();
-
-  prompt('chooseAttack', VALID_CHOICES.join(', '));
+function getUserChoice() {
+  prompt('chooseAttack', Object.values(VALID_CHOICES).join(' '));
   choice = readline.question();
   populateFullChoiceWord(choice);
 
-  while (!VALID_CHOICES.includes(choice)) {
+  while (!Object.values(VALID_CHOICES).includes(choice)) {
     prompt('caseSensative');
     prompt('badInput');
     choice = readline.question();
     populateFullChoiceWord(choice);
   }
+}
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+function getComputerChoice() {
+  let index = Math.floor(Math.random() * Object.keys(VALID_CHOICES).length);
+  computerChoice = Object.values(VALID_CHOICES)[index];
+}
 
+function displayChoices() {
+    prompt('youChose', choice);
+    prompt('computerChose', computerChoice);
+    console.log(); //space for clarity
+}
+
+//repeats upon user request
+while (bestOfFive < 5) {
+  header();
+  getUserChoice();
+  getComputerChoice();
   calculateWinner(choice, computerChoice);
-  tally(winner);
 
-  //display results
-  console.clear();
   header();
   displayWinner(winner);
-  prompt('youChose', choice);
-  prompt('computerChose', computerChoice);
-  console.log(); //space for clarity
+  displayChoices();
 
   //play again?
-  prompt('continue');
-  answer = readline.question();
+  if (bestOfFive === 0 && scoreKeeper.ties <= 1) {
+    prompt('continueBestOf5');
+    answer = readline.question();
+  } else {
+    prompt('continue');
+    answer = readline.question();
+  }
 
   while (answer.trim() !== '' && answer.trim().toLowerCase() !== 'q') {
     prompt('playAgainHelp');
     answer = readline.question().toLowerCase();
   }
 
-  console.clear();
-
   if (answer.trim().toLowerCase() === 'q') break;
   if (winner !== 'tie') bestOfFive += 1;
 }
 
 //display final results
+console.clear();
 prompt('finalScore');
-header();
-if (scoreKeeper[0][1] > scoreKeeper[1][1]) {
+if (scoreKeeper.user > scoreKeeper.computer) {
   displayWinner('user');
-} else if (scoreKeeper[0][1] < scoreKeeper[1][1]) {
+} else if (scoreKeeper.user < scoreKeeper.computer) {
   displayWinner('computer');
 } else {
   displayWinner('tie');
